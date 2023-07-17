7a1785ce7c0f875b22723fd035251a0f0b42c100
Mohit Bishnoi <bishnoi.mohit@icloud.com>
2023-07-18 03:35:03 +1000

    projects added

---
+++ src/Data/data.js
@@ -0,0 +1,37 @@
+import chatapp from '../Assets/chatapp.png'
+import fittrk from '../Assets/fit-trk.png'
+import urlshort from '../Assets/url-short.png'
+import landingpage from '../Assets/landing-page.png'
+
+export const data=[
+    {
+        id:1,
+        name:"Web-Chat Application",
+        image:chatapp,
+        github:"https://github.com/mohitbish/React-chat-app",
+        live:"https://rc-chatapp.onrender.com",
+    },
+    {
+        id:2,
+        name:"Fitness-Track Application",
+        image:fittrk,
+        github:"https://github.com/mohitbish/Fit-Trk",
+        live:"https://fit-trk.onrender.com",
+    },
+    {
+        id:3,
+        name:"Url-Shortener",
+        image:urlshort,
+        github:"https://github.com/mohitbish/React-URL-SHORT",
+        live:"https://url-short-561b.onrender.com",
+    },
+    {
+        id:4,
+        name:"Responsive Landing-Page",
+        image:landingpage,
+        github:"https://github.com/mohitbish/Responsive-Landingpage",
+        live:"https://landing-page-v0sf.onrender.com",
+    },
+
+
+]
